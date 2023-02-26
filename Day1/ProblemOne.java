8e4a6c8ff56218ba5bd1b3d0a25c6625be0011bd
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2023-02-26 20:04:21 +0530

    Day 1 Code Upload

---
+++ Day1/ProblemOne.java
@@ -0,0 +1,12 @@
+class ProblemOne{
+public static void main(String args[]){
+String name = "Joy Chakraborty";
+String branch="Information Technology";
+char batch='B';
+
+System.out.println("\t\t\t Informatiom");
+System.out.println("Name : "+name);
+System.out.println("Branch : "+branch);
+System.out.println("Batch : "+batch);
+}
+}
