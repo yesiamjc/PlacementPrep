79c4e5829c123cb3f2332256d596b8b976542462
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2023-03-19 23:14:22 +0530

    Day 21 & 22

---
+++ Day21/Day21.java
@@ -0,0 +1,5 @@
+class Day21{
+public static void main(String args[]){
+System.out.println("Hey Java");
+}
+}
---
+++ Day22/Day22.java
@@ -0,0 +1,5 @@
+class Day22{
+public static void main(String args[]){
+System.out.println("Hey java ");
+}
+}
